e750f771b4752ad645ded58f565f4d7b7bd96527
Theuy Limpanont <theuy.limpanont@hotmail.com>
2016-01-04 23:01:20 +0000

    Adds when DOM element not found to resolve with .then

--- scripts/load.async.js
+++ scripts/load.async.js
@@ -1,17 +1,44 @@
 define(['Promise'], function(Promise) {
     
+    // Example of another async UI component based on the element's visibility on page
+    /*
+    var loadNewAsyncUIComponent = function() {
+        return new Promis(function(resolve) {
+            var DOM = document.getElementById('someId');
+            if(DOM) {
+                require(['async.ui.component'], function(AsyncUIComponent) {
+                   resolve(new AsyncUIComponent(DOM); return;
+                });
+            }
+            else {
+                resolve(null);
+            }
+        });
+    }
+    */
+    
+    // real case implementation
     var loadRangeSlider = function() {
         return new Promise(function(resolve) {
             var sliderDOM = document.getElementById('rangeSlider');
             if(sliderDOM) {
-               require(["range.slider"], function(RangeSlider) {
-                   resolve(new RangeSlider(sliderDOM));
+               require(['range.slider'], function(RangeSlider) {
+                   console.log("require(['range.slider'] and resolve it");
+                   resolve(new RangeSlider(sliderDOM)); return;
                });  
+            } else {
+                console.log("No need to do require(['range.slider'], because we cannot find the range slider in the DOM");
+                /* 
+                even if we cannot find DOM element we resolve it but with null resolving result
+                we can then later check whether the module has returned a module instance or not
+                */
+                resolve(null); return;
             }
-        })
+        });
     };
     
     return Promise.props({ 
+        // asyncUIComponent: loadNewAsyncUIComponent(),
         rangeSlider: loadRangeSlider()
     });
 });
--- scripts/products.page.js
+++ scripts/products.page.js
@@ -2,12 +2,17 @@ define([
     'loadAsync'
 ], function(loadAsync) {
     
-   loadAsync.then(function(asyncModule) {
-       var rangeSlider = asyncModule.rangeSlider;
-        setInterval(function() {
-            rangeSlider.element.value = (Math.floor(Math.random() * 100));
-            document.getElementById('rangeValue').innerHTML = 'Range Slider value: ' + parseInt(rangeSlider.element.value);
-        }, 800);
+    /*
+    expect this function to always resolve
+    */
+   loadAsync.then(function(asyncModules) {
+       if(asyncModules && asyncModules.rangeSlider) {
+           // do we have a rangeSlider module and can we get the DOM element of the module?
+           var rangeSlider = asyncModules.rangeSlider;
+           setInterval(function() {
+               rangeSlider.element.value = (Math.floor(Math.random() * 100));
+               document.getElementById('rangeValue').innerHTML = 'Range Slider value: ' + parseInt(rangeSlider.element.value);
+           }, 800);       
+       }
    });
-  
 });
